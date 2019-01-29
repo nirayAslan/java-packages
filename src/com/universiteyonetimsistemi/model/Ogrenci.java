0a092e3068ee6a2962b9187253669291379ebd4d
niray <niray.aslan@bulutmd.com>
2019-01-30 01:36:39 +0300

    deneme

---
+++ src/com/universiteyonetimsistemi/model/Ogrenci.java
@@ -0,0 +1,14 @@
+package com.universiteyonetimsistemi.model;
+
+public class Ogrenci {
+
+    private String isim;
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+}
---
+++ src/com/universiteyonetimsistemi/servis/Main.java
@@ -0,0 +1,12 @@
+package com.universiteyonetimsistemi.servis;
+
+import com.universiteyonetimsistemi.model.Ogrenci;
+
+public class Main {
+    public static void main(String[] args) {
+        Ogrenci ali =new Ogrenci();
+
+
+        System.out.println("Merhaba");
+    }
+}
